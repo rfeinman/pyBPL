4f92a974944d2bc48673c519f0e13f755eb157fd
Reuben Feinman <rfeinman16@gmail.com>
2018-06-01 15:29:26 -0400

    update init

--- pybpl/classes/__init__.py
+++ pybpl/classes/__init__.py
@@ -3,10 +3,11 @@ from .spatial_hist import SpatialHist
 from .spatial_model import SpatialModel
 from .stroke import Stroke
 
+from . import CPD
 from . import motor_program
 from . import spatial_hist
 from . import spatial_model
 from . import stroke
 
-__all__ = ['MotorProgram', 'SpatialModel', 'SpatialHist', 'Stroke',
+__all__ = ['CPD', 'MotorProgram', 'SpatialModel', 'SpatialHist', 'Stroke',
            'motor_program', 'spatial_hist', 'spatial_model', 'stroke']
