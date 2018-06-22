5ed0c0a17d8681ddd21ede72e0ea6569348d353f
Reuben Feinman <rfeinman16@gmail.com>
2018-06-22 15:20:21 -0400

    added function placeholders

--- pybpl/classes/UtilMP.py
+++ pybpl/classes/UtilMP.py
@@ -1,3 +1,26 @@
+from __future__ import division, print_function
+
+def clear_shape_type(M):
+    raise NotImplementedError
+
+def clear_invscales_type(M):
+    raise NotImplementedError
+
+def set_affine_to_image(M, I):
+    raise NotImplementedError
+
+def set_affine(M, target_com, target_range, missing):
+    raise NotImplementedError
+
+def set_affine_motor(M, target_com, target_range):
+    raise NotImplementedError
+
+def apply_each_substroke(nested, fnc):
+    raise NotImplementedError
+    nested = None
+
+    return nested
+
 def flatten_substrokes(nested):
     """
     Flatten a nested cell array
@@ -5,4 +28,43 @@ def flatten_substrokes(nested):
     :return:
         vcell:
     """
-    raise NotImplementedError("'flatten_substrokes' not yet implemented.")
+    raise NotImplementedError
+    vcell = None
+
+    return vcell
+
+def flip_stroke(S):
+    raise NotImplementedError
+
+def merge_strokes(S1, S2):
+    raise NotImplementedError
+    Z = None
+
+    return Z
+
+def split_stroke(Z, bid_start_of_second):
+    raise NotImplementedError
+    Z1 = None
+    Z2 = None
+
+    return Z1, Z2
+
+def all_merge_moves(M):
+    raise NotImplementedError
+    moves = None
+    reverse_moves = None
+
+    return moves, reverse_moves
+
+def all_split_moves(M):
+    raise NotImplementedError
+    moves = None
+    reverse_moves = None
+
+    return moves, reverse_moves
+
+def valid_merge(M, sid):
+    raise NotImplementedError
+    val = None
+
+    return val
