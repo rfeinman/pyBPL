5f45c5999381fcb7c32a376abc6de06d63f66823
Reuben Feinman <rfeinman16@gmail.com>
2018-06-22 17:08:14 -0400

    updates to demo_optimize_type script

--- demo_optimize_type.py
+++ demo_optimize_type.py
@@ -1,4 +1,9 @@
+"""
+Sample a character type and then optimize its parameters to maximize the
+likelihood of the type under the prior
+"""
 from __future__ import print_function, division
+import argparse
 import matplotlib.pyplot as plt
 import torch
 
@@ -10,9 +15,9 @@ dtype = torch.float
 # use projected gradient ascent for constrained optimization
 projected_grad_ascent = True
 # learning rate
-lr = 0.01
+lr = 1e-3
 # tol. for constrained optimization
-eps = 0.0001
+eps = 1e-4
 
 def get_variables_MP(mp):
     """
@@ -52,32 +57,44 @@ def obj_fun(mp, lib):
     :return:
         ll: [tensor] log-likelihood under the prior. Scalar
     """
-    # NOTE: don't declare requires_grad
-    ll = torch.tensor(0., dtype=dtype)
+    # start the accumulator
+    ll = 0.
 
     # loop through the strokes
-    # NOTE: must use inline addition
     for sid in range(mp.ns):
         stroke = mp.S[sid]
-        # log-prob of the control points for each sub-stroke in the sequence
+        # log-prob of the control points for each sub-stroke in this stroke
         ll_cpts = CPD.score_shape_type(
             lib, stroke.shapes_type, stroke.ids
         )
-        # log-prob of the scales for this sub-stroke sequence
+        # log-prob of the scales for each sub-stroke in this stroke
         ll_scales = CPD.score_invscale_type(
             lib, stroke.invscales_type, stroke.ids
         )
         # sum over sub-strokes and add to accumulator
-        ll.add_(torch.sum(ll_cpts))
-        ll.add_(torch.sum(ll_scales))
+        ll = ll + torch.sum(ll_cpts) + torch.sum(ll_scales)
 
     return ll
+
+def view_params(mp):
+    """
+    Function to check in on the parameter values
+
+    :param mp:
+    """
+    print('viewing params:')
+    for sid in range(mp.ns):
+        print('\tsub-id: %i' % sid)
+        print('\tshapes_type: ', mp.S[sid].shapes_type)
+        print('\tinvscales_type: ', mp.S[sid].invscales_type)
+        print('\n')
 
 def main():
     # load the library
     lib = Library(lib_dir='./library')
     # generate a motor program (a character type)
-    mp = generate_character(lib)
+    mp = generate_character(lib, ns=args.ns)
+    print('num strokes: %i' % mp.ns)
     # get optimizable variables & their bounds
     parameters, lbs, ubs = get_variables_MP(mp)
 
@@ -86,6 +103,7 @@ def main():
     for idx in range(1000):
         if idx % 100 == 0:
             print('iteration #%i' % idx)
+            #view_params(mp)
         score = obj_fun(mp, lib)
         score.backward()
         score_list.append(score)
@@ -109,4 +127,10 @@ def main():
     plt.show()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--ns', help="The number of strokes",
+        required=False, type=int
+    )
+    args = parser.parse_args()
     main()
