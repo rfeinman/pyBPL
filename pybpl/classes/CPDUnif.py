8416afa752475d419777d120fbc21d0bbac9d35b
Reuben Feinman <rfeinman16@gmail.com>
2018-06-21 21:25:47 -0400

    adding CPDUnif

---
+++ pybpl/classes/CPDUnif.py
@@ -0,0 +1,17 @@
+from __future__ import division, print_function
+
+def sample_shape_type(libclass, subid):
+    # TODO
+    raise NotImplementedError
+
+def score_shape_type(libclass, bspline_stack, subid):
+    # TODO
+    raise NotImplementedError
+
+def sample_invscale_type(libclass, subid):
+    # TODO
+    raise NotImplementedError
+
+def score_invscale_type(libclass, invscales, subid):
+    # TODO
+    raise NotImplementedError
--- pybpl/classes/__init__.py
+++ pybpl/classes/__init__.py
@@ -1,12 +1,13 @@
-from .motor_program import MotorProgram
 from .spatial_hist import SpatialHist
 from .spatial_model import SpatialModel
-from .stroke import Stroke
-from .stroke_type import StrokeType
 from .library import Library
 from .relations import (Relation, RelationAttach, RelationAttachAlong,
                         RelationIndependent)
+from .stroke_type import StrokeType
+from .stroke import Stroke
+from .motor_program import MotorProgram
 
+from . import CPDUnif
 from . import CPD
 from . import motor_program
 from . import spatial_hist
@@ -23,6 +24,6 @@ classes = [
 ]
 modules = [
     'motor_program', 'spatial_hist', 'spatial_model', 'relations', 'CPD',
-    'UtilMP', 'stroke', 'library'
+    'CPDUnif', 'UtilMP', 'stroke', 'library'
 ]
 __all__ = classes + modules
