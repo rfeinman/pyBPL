7df4f4c8e2e5095fd955b2851a994b43e630c608
Reuben Feinman <rfeinman16@gmail.com>
2018-06-23 15:02:28 -0400

    new function score_number to score a stroke count

--- pybpl/classes/CPD/cpd_ns.py
+++ pybpl/classes/CPD/cpd_ns.py
@@ -2,6 +2,7 @@
 Number of strokes model (Kappa)
 """
 from __future__ import division, print_function
+import numpy as np
 import torch
 import torch.distributions as dist
 
@@ -31,7 +32,8 @@ def sample_number(libclass, nsamp=1):
     pkappa = libclass.pkappa
     # get the categorical distribution
     cat = __get_dist(pkappa)
-    # sample from the dist. Add 1 to 0-indexed samples
+    # sample from the dist
+    # NOTE: add 1 to 0-indexed samples
     ns = cat.sample(torch.Size([nsamp])) + 1
     # make sure ns is a vector
     assert len(ns.shape) == 1
@@ -45,9 +47,20 @@ def score_number(libclass, ns):
     Score the log-likelihood of each stroke count in ns
 
     :param libclass: [Library] library class instance
-    :param ns: [(n,) tensor] vector of stroke counts. scalar if n=1
+    :param ns: [(nsamp,) tensor] vector of stroke counts. scalar if nsamp=1
     :return:
-        ll: [(n,) tensor] vector of log-likelihood scores. scalar if n=1
+        ll: [(nsamp,) tensor] vector of log-likelihood scores. scalar if nsamp=1
     """
+    # probability of each stroke count
+    pkappa = libclass.pkappa
+    out_of_bounds = ns > len(pkappa)
+    if out_of_bounds.any():
+        ll = -np.inf
+        return ll
+    # get the categorical distribution
+    cat = __get_dist(pkappa)
+    # score points using the categorical distribution
+    # NOTE: subtract 1 to get 0-indexed samples
+    ll = cat.log_prob(ns-1)
 
-    raise NotImplementedError('not implemented')
+    return ll
