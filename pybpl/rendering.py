446f8ec38d167756c701a01b75580463d2dc805e
Reuben Feinman <rfeinman16@gmail.com>
2018-06-26 13:22:22 -0400

    restructuring rendering.py, adding some functions

--- pybpl/rendering.py
+++ pybpl/rendering.py
@@ -4,32 +4,18 @@ All the functions and modules for differentiable rendering go here
 from __future__ import print_function, division
 import torch
 
+from .general_util import aeq
 from . import splines
 from .concept.part import RenderedPart
 from .character import util_character
 
 
-def com_char(char):
-    raise NotImplementedError
-    ns = len(char)
+# ----
+# vanilla to motor
+# ----
 
 def offset_stk(traj, offset):
     print("'offset_stk' function is unnecessary.")
-    raise NotImplementedError
-
-def space_motor_to_img(pt):
-    raise NotImplementedError
-
-def render_image(cell_traj, epsilon, blur_sigma, PM):
-    raise NotImplementedError
-    traj_img = space_motor_to_img(cell_traj)
-    #TODO
-    ink = PM.ink_pp
-    max_dist = PM.ink_max_dist
-
-    return prob_on, ink_off_page
-
-def sum_pair_dist(D):
     raise NotImplementedError
 
 def vanilla_to_motor(shapes, invscales, first_pos, neval=200):
@@ -66,6 +52,15 @@ def vanilla_to_motor(shapes, invscales, first_pos, neval=200):
 
     return motor, motor_spline
 
+
+
+# ----
+# affine warp
+# ----
+
+def com_char(char):
+    raise NotImplementedError
+
 def affine_warp(stk, affine):
     raise NotImplementedError
 
@@ -84,6 +79,99 @@ def apply_warp(rendered_parts, affine):
         motor_warped = util_character.apply_each_substroke(motor_unwarped, fn)
 
     return motor_warped
+
+
+# ----
+# render the image
+# ----
+
+def check_bounds():
+    pass
+
+def pair_dist():
+    pass
+
+def sub2ind(shape, x, y):
+    pass
+
+def seqadd():
+    pass
+
+def imfilter():
+    pass
+
+def fspecial():
+    pass
+
+def space_motor_to_img(pt):
+    """
+
+    :param pt: [tensor] Fine motor sequence. This is either for an individual
+                sub-stroke, in which case it has shape (neval,2), or for a
+                sequence of sub-strokes, in which case it has shape (k,neval,2)
+    :return:
+    """
+    assert isinstance(pt, torch.Tensor)
+    new_pt = torch.zeros_like(pt, dtype=torch.float)
+    if len(pt.shape) == 2:
+        new_pt[:,0] = -pt[:,1]
+        new_pt[:,1] = pt[:,0]
+    elif len(pt.shape) == 3:
+        new_pt[:,:,0] = -pt[:,:,1]
+        new_pt[:,:,1] = pt[:,:,0]
+    else:
+        raise Exception('pt must be 2- or 3-dim tensor')
+    new_pt = new_pt + 1
+
+    return new_pt
+
+def render_image(cell_traj, epsilon, blur_sigma, parameters):
+    """
+
+    :param cell_traj: [(nsub_total,neval,2) tensor]
+    :param epsilon: [float]
+    :param blur_sigma: [float]
+    :param parameters:
+    :return:
+    """
+    # convert to image space
+    traj_img = space_motor_to_img(cell_traj)
+
+    # get relevant parameters
+    imsize = parameters.imsize
+    ink = parameters.ink_pp
+    max_dist = parameters.ink_max_dist
+    a = parameters.ink_a
+    b = parameters.ink_b
+    ink_ncon = parameters.ink_ncon
+
+    # draw the trajectories on the image
+    template = torch.zeros(imsize, dtype=torch.float)
+    nsub_total = traj_img.shape[0]
+    ink_off_page = False
+    for i in range(nsub_total):
+        pass
+        # check boundaries
+        myt = traj_img[i]
+
+        # compute distance between each trajectory point and the next one
+
+        # make sure we have the minimum amount of ink, if a particular
+        # trajectory is very small
+
+        # share ink with the neighboring 4 pixels
+
+        # paint the image
+
+    # filter the image to get the desired brush-stroke size
+
+
+    return prob_on, ink_off_page
+
+
+# ----
+# apply render
+# ----
 
 def apply_render(rendered_parts, affine, epsilon, blur_sigma, parameters):
     """
