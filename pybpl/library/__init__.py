e8f773836f43c93568491a1add0e502b014e2b25
rfeinman <rfeinman16@gmail.com>
2018-11-08 13:33:41 -0500

    small change to module docstring

--- pybpl/library/__init__.py
+++ pybpl/library/__init__.py
@@ -1,5 +1,5 @@
 """
-The Library module contains a Library class (and its dependencies) for
+The library module contains a Library class (and its dependencies) for
 loading the hyperparameters of a pre-fit BPL model. This is designed primarily
 for the Omniglot use case, enabling the transfer of parameters from the matlab
 BPL repo (see https://github.com/brendenlake/BPL).
