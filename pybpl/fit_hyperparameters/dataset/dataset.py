0b85d1af321cb25a90f1f07d864c7780cb650cab
rfeinman <rfeinman16@gmail.com>
2019-01-08 18:08:36 -0500

    move processing functions into Dataset class

--- pybpl/fit_hyperparameters/dataset/dataset.py
+++ pybpl/fit_hyperparameters/dataset/dataset.py
@@ -1,5 +1,9 @@
 import numpy as np
 from scipy.interpolate import interp1d
+import torch
+
+from ... import splines
+from ..primitives.primitive_classifier import PrimitiveClassifierSingle
 
 
 class Dataset(object):
@@ -28,8 +32,9 @@ class Dataset(object):
         '''
 
         self.names = names
-
         self.substroke_dict = None # need to call self.make_substroke_dict()
+        self.spline_dict = None # need to call self.make_spline_dict()
+        self.subid_dict = None # need to call self.make_subid_dict()
 
         im_0 = images[0][0][0][0][0][0]
         assert im_0.shape==(105,105)
@@ -85,8 +90,7 @@ class Dataset(object):
                             t = times[discrete_step]
                             self.drawings[a][c][r][s][discrete_step] = [x,y,t]
 
-        print("Done building D.drawings.")
-        print("D.drawings[alphabet][character][rendition][stroke][step] is an (x,y,time) tuple")
+        #D.drawings[alphabet][character][rendition][stroke][step] is an (x,y,time) tuple
 
     def check_black_is_true(self,I):
         '''
@@ -99,9 +103,7 @@ class Dataset(object):
         return ((I==True).sum() < (I==False).sum())
 
     def first_stroke_locations(self):
-        
         first_strokes = []
-
         n_alpha = len(self.drawings)
         for a in range(n_alpha):
             alphabet = self.drawings[a]
@@ -117,34 +119,28 @@ class Dataset(object):
                     x,y,t = first_stroke[discrete_step]
                     first_stroke_start = [x,y]
                     first_strokes.append(first_stroke_start)
+        self.first_strokes = np.vstack(first_strokes)
 
-        first_strokes = np.vstack(first_strokes)
-        return first_strokes
-
-
+        return self.first_strokes
 
     def make_substroke_dict(self):
-
-        print("Making substroke dictionary")
-        print("This may take a while... (roughly 3 minutes)")
-    
-        self.substroke_dict = {}
-
+        print("Making sub-stroke dictionary...")
+        substroke_dict = {}
         n_alpha = len(self.drawings)
         for a in range(n_alpha):
-            self.substroke_dict[a] = {}
+            substroke_dict[a] = {}
             alphabet = self.drawings[a]
             n_char = len(alphabet)
             for c in range(n_char):
-                self.substroke_dict[a][c] = {}
+                substroke_dict[a][c] = {}
                 char = alphabet[c]
                 n_rend = len(char)
                 for r in range(n_rend):
-                    self.substroke_dict[a][c][r] = {}
+                    substroke_dict[a][c][r] = {}
                     rendition = char[r]
                     n_stroke = len(rendition)
                     for s in range(n_stroke):
-                        self.substroke_dict[a][c][r][s] = {}
+                        substroke_dict[a][c][r][s] = {}
                         stroke = self.drawings[a][c][r][s]
                         unif_stroke,unif_stroke_times = unif_time(stroke)
                         substrokes,modified_input,breaks = partition_stroke(unif_stroke)
@@ -152,11 +148,86 @@ class Dataset(object):
                         for ss in range(n_substroke):
                             substroke = substrokes[ss]
                             unif_space_substroke = unif_space(substroke)
-                            self.substroke_dict[a][c][r][s][ss] = unif_space_substroke
+                            substroke_dict[a][c][r][s][ss] = unif_space_substroke
+        self.substroke_dict = substroke_dict
 
-        print("Done making substroke dictionary")
-        print("D.substroke_dict[alphabet][character][rendition][stroke][substroke][step] is an (x,y) pair")
+        return self.substroke_dict
+        # D.substroke_dict[alphabet][character][rendition][stroke][substroke][step] is an (x,y) pair
 
+    def make_spline_dict(self):
+        assert self.substroke_dict is not None
+        ss_dict = self.substroke_dict
+        print("Converting sub-strokes to splines...")
+        spline_dict = {}
+        n_alpha = len(ss_dict)
+        for a in range(n_alpha):
+            spline_dict[a] = {}
+            alphabet = ss_dict[a]
+            n_char = len(alphabet)
+            for c in range(n_char):
+                spline_dict[a][c] = {}
+                char = alphabet[c]
+                n_rend = len(char)
+                for r in range(n_rend):
+                    spline_dict[a][c][r] = {}
+                    rendition = char[r]
+                    n_stroke = len(rendition)
+                    for s in range(n_stroke):
+                        spline_dict[a][c][r][s] = {}
+                        stroke = rendition[s]
+                        n_substrokes = len(stroke)
+                        for ss in range(n_substrokes):
+                            num_steps = len(stroke[ss])
+                            if num_steps >= 10:
+                                spline_dict[a][c][r][s][ss] = np.zeros((5, 2))
+                                substk = stroke[ss]
+                                substk, _, scale = norm_substk(substk)
+                                spline = splines.fit_bspline_to_traj(substk,
+                                                                     nland=5)
+                                # PyTorch -> Numpy
+                                spline = spline.numpy()
+                                # Add 2 extra dimensions - scales weighted twice
+                                spline = np.append(spline, [[scale, scale]],
+                                                   axis=0)
+                                spline_dict[a][c][r][s][ss] = spline
+        self.spline_dict = spline_dict
+
+        return self.spline_dict
+
+    def make_subid_dict(self):
+        assert self.spline_dict is not None
+        spline_dict = self.spline_dict
+        clf = PrimitiveClassifierSingle()
+        subid_dict = {}
+        for a in spline_dict.keys():
+            subid_dict[a] = {}
+            for c in spline_dict[a].keys():
+                subid_dict[a][c] = {}
+                for r in spline_dict[a][c].keys():
+                    subid_dict[a][c][r] = {}
+                    for s in spline_dict[a][c][r].keys():
+                        ids = []
+                        for ss in spline_dict[a][c][r][s].keys():
+                            spline = torch.tensor(
+                                spline_dict[a][c][r][s][ss],
+                                dtype=torch.float32
+                            )
+                            prim_ID = clf.predict(spline)
+                            ids.append(prim_ID)
+                        subid_dict[a][c][r][s] = ids
+        self.subid_dict = subid_dict
+
+        return self.subid_dict
+
+
+def norm_substk(substroke, newscale=105):
+    mu = np.mean(substroke, axis=0)
+    substroke = substroke - mu
+    range_x, range_y = np.ptp(substroke, axis=0)
+    scale = newscale / max(1, max(range_x, range_y))
+    substroke = substroke * scale
+
+    return substroke, mu, scale
 
 def unif_time(stroke,time_int=50.0):
 
--- pybpl/fit_hyperparameters/dataset/process_dataset.py
+++ pybpl/fit_hyperparameters/dataset/process_dataset.py
@@ -4,90 +4,8 @@ try:
 except ImportError:
     import cPickle as pickle # python 2.x
 import os
-import numpy as np
 import scipy.io as sio
-import torch
-
-from ... import splines
 from .dataset import Dataset
-from ..primitives.primitive_classifier import PrimitiveClassifierSingle
-
-
-def make_substroke_dict(drawings, images, names, timing):
-    D = Dataset(drawings, images, names, timing)
-    print("Making substroke data...")
-    D.make_substroke_dict()
-
-    return D.substroke_dict
-
-
-def norm_substk(substroke, newscale=105):
-    mu = np.mean(substroke, axis=0)
-    substroke = substroke - mu
-    range_x, range_y = np.ptp(substroke, axis=0)
-    scale = newscale / max(1, max(range_x, range_y))
-    substroke = substroke * scale
-
-    return substroke, mu, scale
-
-
-def make_spline_dict(ss_dict):
-    print("Converting sub-strokes to splines...")
-    spline_dict = {}
-    n_alpha = len(ss_dict)
-    for a in range(n_alpha):
-        spline_dict[a] = {}
-        alphabet = ss_dict[a]
-        n_char = len(alphabet)
-        for c in range(n_char):
-            spline_dict[a][c] = {}
-            char = alphabet[c]
-            n_rend = len(char)
-            for r in range(n_rend):
-                spline_dict[a][c][r] = {}
-                rendition = char[r]
-                n_stroke = len(rendition)
-                for s in range(n_stroke):
-                    spline_dict[a][c][r][s] = {}
-                    stroke = rendition[s]
-                    n_substrokes = len(stroke)
-                    for ss in range(n_substrokes):
-                        num_steps = len(stroke[ss])
-                        if num_steps >= 10:
-                            spline_dict[a][c][r][s][ss] = np.zeros((5,2))
-                            substk = stroke[ss]
-                            substk, _, scale = norm_substk(substk)
-                            spline = splines.fit_bspline_to_traj(substk,nland=5)
-                            # PyTorch -> Numpy
-                            spline = spline.numpy()
-                            # Add 2 extra dimensions - scales weighted twice
-                            spline = np.append(spline,[[scale,scale]],axis=0)
-                            spline_dict[a][c][r][s][ss] = spline
-
-    return spline_dict
-
-
-def make_subid_dict(spline_dict):
-    clf = PrimitiveClassifierSingle()
-    subid_dict = {}
-    for a in spline_dict.keys():
-        subid_dict[a] = {}
-        for c in spline_dict[a].keys():
-            subid_dict[a][c] = {}
-            for r in spline_dict[a][c].keys():
-                subid_dict[a][c][r] = {}
-                for s in spline_dict[a][c][r].keys():
-                    ids = []
-                    for ss in spline_dict[a][c][r][s].keys():
-                        spline = torch.tensor(
-                            spline_dict[a][c][r][s][ss],
-                            dtype=torch.float32
-                        )
-                        prim_ID = clf.predict(spline)
-                        ids.append(prim_ID)
-                    subid_dict[a][c][r][s] = ids
-
-    return subid_dict
 
 
 def preprocess_omniglot(save_dir):
@@ -96,21 +14,22 @@ def preprocess_omniglot(save_dir):
     sd_path = os.path.join(save_dir, 'spline_dict.p')
     sid_path = os.path.join(save_dir, 'subid_dict.p')
 
+    # create the dataset
+    assert os.path.isfile(data_path)
+    print("Loading Data...")
+    data = sio.loadmat(
+        data_path,
+        variable_names=['drawings', 'images', 'names', 'timing']
+    )
+    D = Dataset(data['drawings'],data['images'],data['names'],data['timing'])
+
     # create drawing and substroke dictionaries
     if os.path.isfile(ssd_path):
         print('Sub-stroke dictionary already exists.')
     else:
-        assert os.path.isfile(data_path)
-        print("Loading Data...")
-        data = sio.loadmat(
-            data_path,
-            variable_names=['drawings', 'images', 'names', 'timing']
-        )
-        substroke_dict = make_substroke_dict(
-            data['drawings'], data['images'], data['names'], data['timing']
-        )
+        ss_dict = D.make_substroke_dict()
         with open(ssd_path, 'wb') as fp:
-            pickle.dump(substroke_dict, fp, protocol=pickle.HIGHEST_PROTOCOL)
+            pickle.dump(ss_dict, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
     # create spline dictionary
     if os.path.isfile(sd_path):
@@ -119,7 +38,8 @@ def preprocess_omniglot(save_dir):
         assert os.path.isfile(ssd_path)
         with open(ssd_path, 'rb') as fp:
             ss_dict = pickle.load(fp)
-        spline_dict = make_spline_dict(ss_dict)
+        D.substroke_dict = ss_dict
+        spline_dict = D.make_spline_dict()
         with open(sd_path, 'wb') as fp:
             pickle.dump(spline_dict, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -130,6 +50,7 @@ def preprocess_omniglot(save_dir):
         assert os.path.isfile(sd_path)
         with open(sd_path, 'rb') as fp:
             spline_dict = pickle.load(fp)
-        subid_dict = make_subid_dict(spline_dict)
+        D.spline_dict = spline_dict
+        subid_dict = D.make_subid_dict()
         with open(sid_path, 'wb') as fp:
             pickle.dump(subid_dict, fp, protocol=pickle.HIGHEST_PROTOCOL)
