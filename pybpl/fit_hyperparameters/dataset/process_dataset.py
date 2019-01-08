7ad9f49ce6940776aed107f16a084b367abba32d
rfeinman <rfeinman16@gmail.com>
2019-01-08 18:10:19 -0500

    moving both process_dataset functions into one file

--- pybpl/fit_hyperparameters/dataset/process_dataset.py
+++ pybpl/fit_hyperparameters/dataset/process_dataset.py
@@ -6,6 +6,7 @@ except ImportError:
 import os
 import scipy.io as sio
 from .dataset import Dataset
+from .preprocessed_dataset import PreprocessedDataset
 
 
 def preprocess_omniglot(save_dir):
@@ -54,3 +55,24 @@ def preprocess_omniglot(save_dir):
         subid_dict = D.make_subid_dict()
         with open(sid_path, 'wb') as fp:
             pickle.dump(subid_dict, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def preprocess_omniglot1(save_dir):
+    data_path = os.path.join(save_dir, 'data_background_splines.mat')
+    sid_path = os.path.join(save_dir, 'subid_dict.p')
+
+    if os.path.isfile(sid_path):
+        print('SubID dictionary already exists.')
+        return
+
+    assert os.path.isfile(data_path), "Must first create " \
+                                      "'data_background_splines.mat'"
+    D = sio.loadmat(data_path)
+    dataset = PreprocessedDataset(
+        splines=D['bspline_substks'],
+        drawings=D['pdrawings_norm'],
+        scales=D['pdrawings_scales']
+    )
+    dataset.make_subid_dict()
+    with open(sid_path, 'wb') as fp:
+        pickle.dump(dataset.subids, fp, protocol=pickle.HIGHEST_PROTOCOL)
