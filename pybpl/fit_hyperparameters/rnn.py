721621ddd3eeffb05ac5b35bf89d40d36c003059
rfeinman <rfeinman16@gmail.com>
2019-01-12 13:25:49 -0500

    adding function for training rnn

--- pybpl/fit_hyperparameters/rnn.py
+++ pybpl/fit_hyperparameters/rnn.py
@@ -1,3 +1,4 @@
+import time
 from tqdm import tqdm
 import numpy as np
 import tensorflow as tf
@@ -19,7 +20,7 @@ def preprocess_sequences(seqs, vocab_size, max_len, one_hot_targets=True):
 
     return X, Y
 
-def predict(model, X, sess, batch_size=128):
+def predict(sess, model, X, batch_size=128):
     # build TF graph
     x = tf.placeholder(dtype=tf.int32, shape=(None,)+X.shape[1:])
     y_pred = model(x)
@@ -38,3 +39,68 @@ def predict(model, X, sess, batch_size=128):
     Y_pred = np.concatenate(Y_pred)
 
     return Y_pred
+
+def validate_epoch(sess, x, y, loss, X, Y, batch_size):
+    nb_batches = int(np.ceil(X.shape[0] / batch_size))
+    losses = []
+    for i in range(nb_batches):
+        X_batch = X[i*batch_size:(i+1)*batch_size]
+        Y_batch = Y[i*batch_size:(i+1)*batch_size]
+        loss_val = sess.run(
+            loss,
+            feed_dict={x: X_batch, y: Y_batch, K.learning_phase(): 0}
+        )
+        losses.append(loss_val)
+
+    return np.mean(losses)
+
+def train_epoch(sess, x, y, loss, train_op, X, Y, batch_size):
+    nb_batches = int(np.ceil(X.shape[0] / batch_size))
+    losses = []
+    for i in tqdm(range(nb_batches)):
+        X_batch = X[i*batch_size:(i+1)*batch_size]
+        Y_batch = Y[i*batch_size:(i+1)*batch_size]
+        loss_val, _ = sess.run(
+            [loss, train_op],
+            feed_dict={x: X_batch, y: Y_batch, K.learning_phase(): 1}
+        )
+        losses.append(loss_val)
+
+    return np.mean(losses)
+
+def train(sess, model, X, Y, loss_fn, epochs, validation_split=0.2,
+          batch_size=32, save_file=None):
+    x = tf.placeholder(dtype=tf.int32, shape=(None,)+X.shape[1:])
+    y = tf.placeholder(dtype=tf.float32, shape=(None,)+Y.shape[1:])
+    y_pred = model(x)
+    loss = loss_fn(x, y, y_pred)
+
+    # create optimizer
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+    train_op = optimizer.minimize(loss, var_list=model.trainable_weights)
+
+    # train-valid split
+    assert X.shape[0] == Y.shape[0]
+    assert validation_split > 0.01
+    nb_train = int(X.shape[0]*(1.-validation_split))
+    X_train, Y_train = X[:nb_train], Y[:nb_train]
+    X_valid, Y_valid = X[nb_train:], Y[nb_train:]
+
+    # train the model
+    train_losses = []
+    valid_losses = []
+    with sess.as_default() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(epochs):
+            print('Epoch # %i/%i' % (epoch+1, epochs))
+            time.sleep(0.2)
+            train_loss = train_epoch(sess, x, y, loss, train_op, X_train, Y_train, batch_size)
+            time.sleep(0.2)
+            valid_loss = validate_epoch(sess, x, y, loss, X_valid, Y_valid, batch_size)
+            print('train_loss: %0.4f - valid_loss: %0.4f\n' % (train_loss, valid_loss))
+            if valid_loss < np.min(valid_losses):
+                model.save(save_file)
+            train_losses.append(train_loss)
+            valid_losses.append(valid_loss)
+
+    return model, train_losses, valid_losses
