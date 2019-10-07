cc59c22d4457757cb20f03b313dcdd14ad016c64
rfeinman <rfeinman16@gmail.com>
2019-10-07 18:00:28 -0400

    removing all learning-to-learn code for now. This was unused

--- pybpl/fit_hyperparameters/primitives/__init__.py
+++
@@ -1,4 +0,0 @@
-from .primitive_classifier import PrimitiveClassifierSingle
-from .primitive_classifier import PrimitiveClassifierBatch
-
-__all__ = ["PrimitiveClassifierSingle", "PrimitiveClassifierBatch"]
