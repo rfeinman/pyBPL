2f1dbb2cd662f849eb3b00644897a81b076285dd
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2019-10-23 23:50:57 -0400

    option to plot loss when training with small lib

--- amortized_bpl/plot_loss.py
+++ amortized_bpl/plot_loss.py
@@ -2,9 +2,16 @@ import matplotlib.pyplot as plt
 import model
 
 
-if __name__ == '__main__':
-    bpl = model.BPL()
-    bpl.load_inference_network('save/bpl_inference_network')
+def main(args):
+    if args.small_lib:
+        lib_dir = '../lib_data250'
+        save_path_suffix = '_250'
+    else:
+        lib_dir = '../lib_data'
+        save_path_suffix = ''
+    bpl = model.BPL(lib_dir=lib_dir)
+    bpl.load_inference_network('save/bpl_inference_network{}'.format(
+        save_path_suffix))
 
     trace_per_second = bpl._inference_network._total_train_traces / \
         bpl._inference_network._total_train_seconds
@@ -17,6 +24,16 @@ if __name__ == '__main__':
 
     fig.tight_layout(pad=0)
 
-    filename = 'plots/loss.pdf'
+    filename = 'plots/loss{}.pdf'.format(save_path_suffix)
     fig.savefig(filename, bbox_inches='tight')
     print('Saved to {}'.format(filename))
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--small-lib', action='store_true',
+                        help='use 250 primitives')
+    args = parser.parse_args()
+    main(args)
