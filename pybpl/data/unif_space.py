40d9fb770598f5568816c278de3306121b50a737
rfeinman <rfeinman16@gmail.com>
2020-05-01 12:55:49 -0400

    refactor unif_space for speedup

--- pybpl/data/unif_space.py
+++ pybpl/data/unif_space.py
@@ -9,16 +9,16 @@ def unif_space(stroke, dist_int=1.):
 
     Parameters
     ----------
-    stroke : (n,2) array
-        input stroke
+    stroke : np.ndarray
+        (n,2) input stroke
     space_int : float
         space interval; we want approximately this much euclidean distance
         covered between successive points for spatial interpolation
 
     Returns
     -------
-    new_stroke : (m,2) array
-        interpolated stroke
+    new_stroke : np.ndarray
+        (m,2) interpolated stroke
     """
     num_steps = len(stroke)
 
@@ -26,22 +26,14 @@ def unif_space(stroke, dist_int=1.):
     if len(stroke) == 1:
         return stroke
 
-    # compute distance between each point
-    dist = np.zeros((num_steps,1))
-    to_remove = np.zeros((num_steps,1), dtype=bool) # array of false
-
-    for i in range(1,num_steps):
-        xy_1 = stroke[i]
-        xy_2 = stroke[i-1]
-        diff = xy_1 - xy_2
-        dist[i] = np.linalg.norm(diff)
-        to_remove[i] = dist[i] < 1e-4
-
-    remove_indices = [i for i,b in enumerate(to_remove) if b==True]
-
-    # remove points that are too close
-    stroke = np.delete(stroke,remove_indices,axis=0)
-    dist = np.delete(dist,remove_indices,axis=0)
+    # compute distance between each point &
+    # remove points that are too close to previous
+    dist = np.zeros(num_steps)
+    dist[1:] = np.linalg.norm(stroke[1:] - stroke[:-1], axis=-1)
+    remove = np.zeros(num_steps, dtype=bool)
+    remove[1:] = dist[1:] < 1e-4
+    stroke = stroke[~remove]
+    dist = dist[~remove]
 
     # return if stroke is too short
     if len(stroke) == 1:
@@ -49,16 +41,15 @@ def unif_space(stroke, dist_int=1.):
 
     # cumulative distance
     cumdist = np.cumsum(dist)
-    start_dist = cumdist[0]
-    end_dist = cumdist[-1]
-    nint = round(end_dist/dist_int)
-    nint = int(max(nint,2))
-    fx = interp1d(cumdist,stroke[:,0])
-    fy = interp1d(cumdist,stroke[:,1])
+    total_dist = cumdist[-1]
+    nint = int(round(total_dist/dist_int))
+    nint = max(nint, 2)
+    fx = interp1d(cumdist, stroke[:,0])
+    fy = interp1d(cumdist, stroke[:,1])
 
     # new stroke
-    query_points = np.linspace(start_dist,end_dist,nint,endpoint=True)
-    new_stroke = np.zeros((len(query_points),2))
+    query_points = np.linspace(0, total_dist, nint)
+    new_stroke = np.zeros((nint,2))
     new_stroke[:,0] = fx(query_points)
     new_stroke[:,1] = fy(query_points)
 
