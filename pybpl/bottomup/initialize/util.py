4656add3447eeccc8aa4fcd9d4a28c45c706415c
rfeinman <rfeinman16@gmail.com>
2020-05-14 14:51:09 -0400

    adding implementation of stroke_from_nodes

--- pybpl/bottomup/initialize/util.py
+++ pybpl/bottomup/initialize/util.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+
+
 def fit_smooth_stk(stk, image, ps):
     raise NotImplementedError
 
@@ -18,4 +22,22 @@ def stroke_from_nodes(graph, list_ni):
     """
     Compute stroke trajectory from a graph and a list of nodes
     """
-    raise NotImplementedError
+    num_nodes = len(list_ni)
+    if num_nodes == 1:
+        # edge case: single-point stroke
+        pt = graph.nodes[list_ni[0]]['o']
+        return pt[None]
+    stroke = []
+    for i in range(1, num_nodes):
+        curr_ni = list_ni[i-1]
+        curr_pt = graph.nodes[curr_ni]['o']
+        next_ni = list_ni[i]
+        traj = graph.edges[curr_ni, next_ni]['pts']
+        d_start = np.linalg.norm(curr_pt - traj[0])
+        d_end = np.linalg.norm(curr_pt - traj[-1])
+        if d_end < d_start: # flip the traj if needed
+            traj = np.flip(traj, 0)
+        traj = np.insert(traj, 0, curr_pt, axis=0)
+        stroke.append(traj)
+    stroke = np.concatenate(stroke)
+    return stroke
