fe378e751e3aaf7b176a3516328886fd9a2e5b35
rfeinman <rfeinman16@gmail.com>
2020-10-25 22:28:55 -0400

    improve skeleton graph extraction - use binary erosion and multigraphs

--- pybpl/bottomup/skeleton/extract_skeleton.py
+++ pybpl/bottomup/skeleton/extract_skeleton.py
@@ -8,8 +8,9 @@ from .sknw import build_sknw
 def extract_skeleton(image):
     # get thinned image (skeleton)
     image = morphology.remove_small_holes(image, 2)
+    image = morphology.binary_erosion(image)
     image = morphology.thin(image)
     # convert skeleton into network of junction nodes (undirected graph)
-    graph = build_sknw(image.astype(np.uint16))
+    graph = build_sknw(image.astype(np.uint16), multi=True)
 
     return graph
