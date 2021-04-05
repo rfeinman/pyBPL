44e8935bd772d66d03b6beb699859e3842347553
rfeinman <rfeinman16@gmail.com>
2021-04-05 18:39:10 -0400

    fix categorical probability normalization

--- pybpl/library/spatial_OLD/spatial_hist.py
+++ pybpl/library/spatial_OLD/spatial_hist.py
@@ -71,7 +71,7 @@ class SpatialHist(object):
         logN = torch.log(N)
 
         # Convert to probability distribution
-        logpN = logN - torch.logsumexp(logN, 0)
+        logpN = logN - torch.logsumexp(logN.view(-1), 0)
         assert aeq(torch.sum(torch.exp(logpN)), torch.tensor(1.))
 
         self.logpYX = logpN
